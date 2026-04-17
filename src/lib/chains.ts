dd9b94c26d2c660697943b3190eccdb50b6a6280
Chronosopher <122742439+PatoSuave@users.noreply.github.com>
2026-04-16 22:15:14 -0400

    Add files via upload

---
+++ src/lib/chains.ts
@@ -0,0 +1,20 @@
+export const pulsechain = {
+  id: 369,
+  name: "PulseChain",
+  nativeCurrency: {
+    name: "Pulse",
+    symbol: "PLS",
+    decimals: 18,
+  },
+  rpcUrls: {
+    default: {
+      http: [process.env.NEXT_PUBLIC_PULSECHAIN_RPC_URL || "https://rpc.pulsechain.com"],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: "PulseScan",
+      url: "https://scan.pulsechain.com",
+    },
+  },
+} as const;
